0ea4ef5d6d0220e4cfa9f6a41d32ae7f26227356
Darshan Bhabad <darshanscode7@gmail.com>
2026-06-01 23:24:35 +0530

    Time: 0 ms (100%), Space: 24.3 MB (64.83%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -13,19 +13,42 @@ class Solution {
 public:
           //APPROACH 1
    
-    void inorder(TreeNode* root, vector<int>& inor){
-        if(root == NULL) return;
+    // void inorder(TreeNode* root, vector<int>& inor){
+    //     if(root == NULL) return;
 
-        inorder(root->left, inor);
-        inor.push_back(root->val);
-        inorder(root->right, inor);
+    //     inorder(root->left, inor);
+    //     inor.push_back(root->val);
+    //     inorder(root->right, inor);
+    // }
+
+    // int kthSmallest(TreeNode* root, int k) {
+    //     vector<int> inor;
+    //     inorder(root, inor);
+
+    //     return inor[k - 1];
+
+//Approach 2 more efficirnt stop at kth node;
+int ans;
+
+void inorder(TreeNode* root,int &k){
+    if(root==NULL) return;
+    inorder(root->left,k);
+    k--;
+    if(k==0){ //that much values are covered or traversed
+    ans=root->val;
+    return;
+
     }
 
-    int kthSmallest(TreeNode* root, int k) {
-        vector<int> inor;
-        inorder(root, inor);
+    inorder(root->right,k);
+    return;
+}
 
-        return inor[k - 1];
+ int kthSmallest(TreeNode* root, int k) {
+        // vector<int> inor;
+        inorder(root, k);
+
+        return ans;
         
     }
 };
