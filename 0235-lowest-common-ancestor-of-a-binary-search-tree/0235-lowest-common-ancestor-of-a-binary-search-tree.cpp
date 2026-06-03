77ce05d2bfd3e9fd8ca79a4439c48929ba0f9560
Darshan Bhabad <darshanscode7@gmail.com>
2026-06-04 01:41:57 +0530

    Time: 24 ms (49.6%), Space: 23.6 MB (33.99%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -13,17 +13,32 @@ public:
 
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         
-        if(root==NULL) return NULL;
+    //     if(root==NULL) return NULL;
        
-        if(root ==p || root==q) return root;
-       TreeNode* l= lowestCommonAncestor( root->left,  p,  q);
-       TreeNode* r= lowestCommonAncestor( root->right,  p,  q);
+    //     if(root ==p || root==q) return root;
+    //    TreeNode* l= lowestCommonAncestor( root->left,  p,  q);
+    //    TreeNode* r= lowestCommonAncestor( root->right,  p,  q);
     
-    if(l && r) return root; //both are not  null;
+    // if(l && r) return root; //both are not  null;
 
-    if(l!=NULL) return l;
-    else
-    return r;
+    // if(l!=NULL) return l;
+    // else
+    // return r;
+
+   // APPROACH 2
+   if(root ==NULL) return NULL;
+   if(root->val>p->val && root->val > q->val) //left subtree has p and q
+   return lowestCommonAncestor( root->left,  p,  q);
+
+   if(root->val<p->val && root->val < q->val) //right subtree has p and q
+   return lowestCommonAncestor( root->right,  p,  q);
+
+   else{ //LCA is p itself or q itself and if they both are in diff subtrees then root itself
+   return root;
+     
+
+   }
+
         
     }
 };
