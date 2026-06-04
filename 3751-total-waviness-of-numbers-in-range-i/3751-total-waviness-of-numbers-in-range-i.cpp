17b607f1ffa3db169925dee13f9e839e408559b6
Darshan Bhabad <darshanscode7@gmail.com>
2026-06-04 12:41:15 +0530

    Time: 55 ms (33.96%), Space: 10.2 MB (42.54%) - LeetHub

--- 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.cpp
+++ 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.cpp
@@ -3,7 +3,7 @@ public:
 int countwavi(int num){
     string p= to_string(num);
     int cnt=0;
-    if(p.size()<0) return 0;
+    if(p.size()<3) return 0; //optimization as if curr i is less tha 3 size wavi is 0;
     for(int i=1;i<p.size()-1;i++){
         // if((p[i-1]-'0'<p[i]-'0'>p[i+1]-'0') ||(p[i-1]-'0'>p[i]-'0'<p[i+1]-'0')) cnt++; doesnt support like paython a<b>c evaluated as (a>b)>c
         //so
@@ -18,7 +18,7 @@ int countwavi(int num){
 }
     int totalWaviness(int num1, int num2) {
         string s=to_string(num2);
-        if(s.size()<3) return 0; //optiization
+        if(s.size()<3) return 0; //optimization as if num2 is less than 3 digit num1 has to be so wavi 0
 int wavi=0;
         for(int i=num1;i<=num2;i++){
 
