4cdc29412c36e830b478d1cfaa48a481d1c500db
Darshan Bhabad <darshanscode7@gmail.com>
2026-06-13 08:27:22 +0530

    Time: 0 ms (100%), Space: 43.7 MB (24.57%) - LeetHub

---
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+    
+    int j=0;
+    string ans;
+    for(int i=0;i<words.size();i++){
+        int sum=0;
+
+         for(char c: words[i]){
+            sum+=weights[(c-'a')];
+         }
+        sum=sum % 26;
+        ans+=('z'-sum);
+        
+
+
+    }
+        return ans;
+    }
+};
