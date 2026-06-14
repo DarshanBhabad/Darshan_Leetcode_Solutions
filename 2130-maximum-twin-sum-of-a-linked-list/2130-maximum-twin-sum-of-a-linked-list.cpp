d8b0a0df6b2fad78739fdb4f92e20707b4d70326
Darshan Bhabad <darshanscode7@gmail.com>
2026-06-15 00:13:39 +0530

    Time: 0 ms (100%), Space: 148 MB (5.01%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -26,5 +26,49 @@ int sum=0;
 
         }
 return sum;
+
+//APPROACH 2 ... NO vector , SC= O(1);
+// twin nodes 1st node from start 1st node from end 2nd node from start is twin of 2nd node from last 
+// i th node (n-1-i)th node;
+//as number od nodes are even 
+
+//  ListNode* middleNode(ListNode* head){
+//         ListNode* slow = head;
+//         ListNode* fast = head;
+
+//         while(fast->next && fast->next->next){
+//             slow = slow->next;
+//             fast = fast->next->next;
+//         }
+
+//         return slow;
+//     }
+
+//     ListNode* reverse(ListNode* head){
+//         ListNode* prev = NULL;
+//         ListNode* curr = head;
+//         while(curr){
+//             ListNode* temp = curr->next;
+//             curr->next = prev;
+//             prev = curr;
+//             curr = temp;
+//         }
+//         return prev;
+//     }
+
+//     int pairSum(ListNode* head) {
+//         ListNode* middle = middleNode(head);
+//         ListNode* second = middle->next;
+//         middle->next = NULL;
+//         ListNode* first = reverse(head);  //we reversed the 1st part of list by finding middle node  then storing all second parts nodes in 2nd  then setting 1st part last node that is middle node of whole list as NULL and reversing the the 1st part (either you can reverse 1st part or 2nd part)
+//         int res = 0;
+//         while(first){
+//             res = max(res,first->val + second->val);
+//             first = first->next;
+//             second = second->next;
+//         }
+//         return res;
+//     }
+// };
     }
 };
