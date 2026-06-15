ebc8db484e718257b257dac9c59d23a92d0ec66d
Darshan Bhabad <darshanscode7@gmail.com>
2026-06-15 14:07:46 +0530

    Time: 0 ms (100%), Space: 312.1 MB (54.78%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
@@ -19,7 +19,7 @@ public:
 
         ListNode* slow=head;
         ListNode* fast= head;
-        ListNode* prev=slow;
+        ListNode* prev;
 
         while(fast && fast->next){ //fast !=NULL and fast->next!=NULL as we incrementing fast twice so to avoid NULL->next error;
         prev=slow;
@@ -29,7 +29,28 @@ public:
         //now slow will point to middle node and prev will point node prev of middle
         prev->next=slow->next;
         delete(slow);
-        // if(head==NULL) return NULL;
+        
         return head;
+
+
+        //OR dont have to use prev pntr
+
+        
+//        class Solution {
+// public:
+//     ListNode* deleteMiddle(ListNode* head) {
+//         ListNode* fast = head, *slow = head;
+
+//         if (fast->next == NULL) return NULL;
+//         fast = fast->next->next;  //increment fast before loop only so after loop slow will point to node before the middle node
+//         while (fast != NULL && fast->next != NULL) {
+//             fast = fast->next->next;
+//             slow = slow->next;
+//         }
+
+//         slow->next = slow->next->next;  //now slow ehich is prev node of middle sets it next as     n       middle's   (slow->next)-> next
+//         return head;
+//     }
+// };
     }
 };
