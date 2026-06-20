16bd6da0f6de97c025fb65015033b5dcc19af27b
Darshan Bhabad <darshanscode7@gmail.com>
2026-06-21 00:32:10 +0530

    Time: 3 ms (70.83%), Space: 33.2 MB (8.1%) - LeetHub

---
+++ 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class BSTIterator {
+public:
+    //TreeNode* next;
+    
+    int nextptr;
+    vector<TreeNode*>v;
+
+
+    void inorder(TreeNode* root){
+    if(root==NULL) return ;
+    inorder(root->left);
+    v.push_back(root);
+    inorder(root->right);
+    return ;
+}
+    BSTIterator(TreeNode* root) { //constructor of class BST iterator actual mem allocation happens here
+       
+      //O(n)
+      nextptr=-1;   // as values of node are greater than 0
+      inorder(root);
+        //for specific object nextptr values will be maintained
+    }
+
+
+    int next() { //O(1)
+        nextptr++;
+     
+     return v[nextptr]->val;
+
+
+        
+    }
+    
+    bool hasNext() { //O(1)
+    
+       return nextptr+1<v.size(); //we dont have to update nextptr just check
+        
+    }
+};
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator* obj = new BSTIterator(root);
+ * int param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */
