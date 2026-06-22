7d5eec9b9f597b7453acebe6bac4514f63a2e1c8
Darshan Bhabad <darshanscode7@gmail.com>
2026-06-22 14:40:49 +0530

    Time: 0 ms (100%), Space: 8.9 MB (12.43%) - LeetHub

--- 2287-rearrange-characters-to-make-target-string/2287-rearrange-characters-to-make-target-string.cpp
+++ 2287-rearrange-characters-to-make-target-string/2287-rearrange-characters-to-make-target-string.cpp
@@ -14,7 +14,13 @@ public:
     }
 int ans=INT_MAX;
     for(char c :target){
-      ans=min(ans,m[c]/t[c]);
+      ans=min(ans,m[c]/t[c]);  // refer que 1189 
+      // suppose you need i.e in target word have 2l and s has 3l so req target will form only once as 
+      // for single target to form we need 2 occu of l 
+      // so we devide occu of letter in s by occu of it in target 
+      //if target's letter isn't in s it will create m[c] with 0 freq;
+      // we store min occu
+
     }
         return ans;
     }
