c68ba778ac40c355fb05bc2af1209bb165c6c663
Darshan Bhabad <darshanscode7@gmail.com>
2026-06-25 14:17:55 +0530

    Time: 407 ms (11.11%), Space: 40 MB (41.39%) - LeetHub

--- 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.cpp
+++ 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.cpp
@@ -19,5 +19,48 @@ public:
            
         }
         return ans;
+
+
+
+        //Approach 2 
+        //no extra space
+//         class Solution {
+// public:
+//     int countMajoritySubarrays(vector<int>& nums, int target) 
+//     {
+        // First method
+
+        // Declarations and initializations
+        // int i, j, n, count, temp, ans;
+        // n = nums.size();
+        // ans = 0;
+
+        // Actual logic
+//         for(i=0; i<n; i++)
+//         {
+//             count = 0;
+//             for(j=i; j<n; j++)
+//             {
+//                 if(nums[j] == target)
+//                 count++;
+
+//                 temp = j - i + 1;
+//                 if(temp == 1)
+//                 {
+//                     if(count == 1)
+//                     ans++;
+//                 }
+//                 else
+//                 {
+//                     if(count > temp/2)
+//                     ans++;
+//                 }
+//             }
+//         }
+
+//         // Return answer
+//         return ans;
+//     }
+// };
     }
 };
