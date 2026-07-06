0072fcd7379a150b69d57561ec009eafb8cbebaa
Darshan Bhabad <darshanscode7@gmail.com>
2026-07-06 15:18:17 +0530

    Time: 0 ms (100%), Space: 15.2 MB (60.13%) - LeetHub

---
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+
+        // Time Complexity: O(n log n) (sorting dominates)
+
+
+        //custome sort func to sort intervals by asc order of st time
+        sort(intervals.begin(), intervals.end(), [](const  vector<int>& a, const vector<int>& b) {
+            if(a[0] == b[0])
+        return a[1] > b[1];   // larger end first   smae st time [1,4],[1,5]
+        return a[0] < b[0]; 
+        });
+        // const tells the compiler: "I want to read this data via reference to save memory, and I promise not to change it."   // refer to the mem that is allocated in interval vector only
+    
+    vector<vector<int>>ans;
+    ans.push_back(intervals[0]); //insert 1st element(vector)
+
+    for(int i=1;i<intervals.size();i++){
+        // int n=ans.size(); //roews in ans
+        if(intervals[i][1] > ans.back()[1])    //last  inseted elemnt  sorted order do st time will be lesser always
+        {
+            ans.push_back({min(intervals[i][1],ans.back()[1]),max(intervals[i][1],ans.back()[1])});
+            // pr jusr insert ans.push_back(intervals[i]);  as it will also track end time 
+        }
+    }
+
+    
+return ans.size();
+        
+    }
+};
