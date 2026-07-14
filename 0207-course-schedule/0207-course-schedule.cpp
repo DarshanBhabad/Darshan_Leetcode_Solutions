f2343fed214af22b0b292dbd8529d25d8c45c89d
Darshan Bhabad <darshanscode7@gmail.com>
2026-07-14 16:58:13 +0530

    Time: 8 ms (34.25%), Space: 20.4 MB (13.17%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+ 
+ bool dfscycle(int src,vector<bool>& vis,vector<list<int>>& G,vector<bool>& recpath){
+   vis[src]=true;
+   recpath[src]=true;
+
+   for(int v:G[src]){
+    if(!vis[v]){
+        if(dfscycle(v,vis,G,recpath)) return true;
+       
+    }
+     else if(recpath[v]) return true;
+    
+    
+   }
+   recpath[src]=false;//backtracking
+    
+return false;
+ }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        //prerequisites are the graph eges , graph is acyclic and directed 
+        //use top[logical sorting 
+        //[ai,bi]= bi->ai  ai is dependnd on bi so bi is depenent of ai
+
+        //check cycle in directed graph using dfs if cycle exist topological ordering not possible : return false
+
+        vector<bool>vis(numCourses,false);
+        // stack<int>s;
+
+        vector<list<int>>G(numCourses); //graph
+        for(auto p:prerequisites){
+            G[p[1]].push_back(p[0]); //[ai,bi] ---bi->ai
+            
+        }
+vector<bool> recpath(numCourses, false); //declared once becoz  it automatically becomes empty or set false once we complete func call
+        for(int i=0;i<numCourses;i++){
+            if(!vis[i]){              
+                if(dfscycle(i,vis,G,recpath)) return false; //cycle detected
+
+            }
+        }
+return true;      
+    }
+};
