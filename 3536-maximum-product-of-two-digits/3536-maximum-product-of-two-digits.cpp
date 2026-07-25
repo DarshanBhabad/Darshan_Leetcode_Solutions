6541cfe67c7429ecf0a0b30794da83d69024a2d9
Darshan Bhabad <darshanscode7@gmail.com>
2026-07-25 11:48:16 +0530

    Time: 0 ms (100%), Space: 9 MB (29.61%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int maxProduct(int n) {
+        //TC=d(logd) d= number of digits
         //we need 2 max digits from the number so that the product will be maximum
         //so use priority queue
         priority_queue<int>q; //max heap max element at root
@@ -12,5 +13,17 @@ public:
         q.pop();
         int b=q.top();
         return a*b;
+
+
+// class Solution {
+//    public:
+//     int maxProduct(int n) {
+        //TC==logn
+//         string s = to_string(n); //Tc= \(O(\log_{10} n)\)
+//         sort(s.rbegin() , s.rend()); // logn
+//         int maxProduct = (s[0]-'0')*(s[1]-'0');
+//         return maxProduct;
+//     }
+// };
     }
 };
