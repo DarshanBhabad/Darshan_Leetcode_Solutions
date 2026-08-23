5c4e85adf42e47d7a18de8bd240ff81d47b68e42
Darshan Bhabad <darshanscode7@gmail.com>
2026-08-23 16:57:30 +0530

    Time: 3 ms (66.67%), Space: 14 MB (33.57%) - LeetHub

--- 1927-sum-game/1927-sum-game.cpp
+++ 1927-sum-game/1927-sum-game.cpp
@@ -37,10 +37,20 @@ public:
     // Difference in ? between the two halves
         int diffQ = abs(ql - qr);
 
-       // Alice/Bob can compensate for this difference
-        int requiredDiff = 9 * (diffQ / 2);
+       // Alice/Bob can compensate for this difference both choosing 9 ,9 in respective tuns in opposite halfs
 
-        return suml - sumr != 9 * (qr - ql) / 2;
+
+       //now q's that are in the same half  // alice always choose 9 and bob 0 to oppose each other 
+        int requiredDiff = 9 * (diffQ / 2);  //dffQ/2 is the total  pairs  each pair gives 9
+
+        return suml - sumr != 9 * (qr - ql) / 2; //(if true then alice win as cant make both sum equal)   // so we track respective halfs
+
+//         suml - sumr = 9  
+// 9 * (qr - ql) / 2   
+// = 9 * (1 - 3) / 2    
+// = -9  //  9!=-9 alice wins  these difference should be covered by one who has large value 
+//here suml was large  and qr were less that is 1  so again left contains 1 extra pair 
+// so it only make  suml big by 9 & not increase sumr to match suml
 
     }
 };
