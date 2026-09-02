d2b07537bc6e699c9d48c41e2d8877f2b7cfcc4b
Darshan Bhabad <darshanscode7@gmail.com>
2026-09-02 14:34:56 +0530

    Time: 0 ms (100%), Space: 30.3 MB (67.8%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        int n=nums1.size();
+        // If the array contains at most one odd number, the answer is true. Otherwise, it's false
+        
+
+        
+//
+// If all elements are odd → already uniform → true
+// If all elements are even → already uniform → true
+// If there are both odd and even numbers, the answer depends on whether the required operation can make them all equal.
+
+return true; 
+
+
+//ans will be always true 
+// if odd and veen both present use odd number as a subtractor and keep the oddd number
+//odd number will make all evbens odd and we keep all odds same
+        
+
+    }
+};
