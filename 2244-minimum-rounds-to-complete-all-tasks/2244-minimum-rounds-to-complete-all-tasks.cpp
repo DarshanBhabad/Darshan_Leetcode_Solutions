c39d840841d93072cb1efd5073d0fc0a7fd1375d
Darshan Bhabad <darshanscode7@gmail.com>
2026-09-23 17:59:20 +0530

    Time: 20 ms (96.01%), Space: 107.5 MB (51.15%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -13,26 +13,33 @@ public:
     for(auto p:freq){
          int v = p.second;
 
+         //choice 1 selectimg 3 
+
             // Single task cannot be completed
             if (v == 1) return -1;
 
-            int d3 = v / 3;
+            int d3 = v / 3;  
             int rem3 = v % 3;
 
-            if (rem3 == 1) {
+            if (rem3 == 1) { //not possible to take in grps of 3  // so 
+            // remove one grp of 3  so these grps 3 + 1 of remainder = 4    
+            // now choose them in grps of 2  so d3-- and tehn d3+=2 
                 d3--;
                 d3 += 2;
             }
-            else if (rem3 == 2) {
-                d3++;
+            else if (rem3 == 2) { // less than 3 v=2
+                d3++;  //0+2 or if  v=5  d3=1  and d3++=2  whcih adds choosing remanining 2 in next rnd 
             }
+
+            //chjoice 2 celectimng in grps of 2
 
             int d2 = v / 2;
             int rem2 = v % 2;
 
-            if (rem2 == 1) {
+            if (rem2 == 1) {  // ex v=5   so reduce one d2 choice so it will give 2 and rekmainder 1 
+            // adds upto 3  so d2-- and d2+=1  
                 d2--;
-                d2 += 2;
+                d2 += 1;
             }
 
             ans += min(d3, d2);
