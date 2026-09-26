a2dc4453859d31066b0b5181717efd8c3d71c97a
Darshan Bhabad <darshanscode7@gmail.com>
2026-09-27 01:27:50 +0530

    Time: 58 ms (10.46%), Space: 75.8 MB (18.71%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -8,41 +8,68 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
- //QUICK SORT
 class Solution {
 public:
-    ListNode* sortList(ListNode* head) {
+
+ListNode* merge(ListNode* L1,ListNode* L2){
+    ListNode* d=new ListNode(0);
+    ListNode* temp = d;
+
+    
+    while(L1 && L2){ //if anyone of tehm become sNULL stop
+          if(L1->val<=L2->val){ //both L1 and L2 are already sorted 
+            temp->next=L1;
+            L1=L1->next;
+          }
+          else{
+             temp->next = L2;
+                L2 = L2->next;
+          }
+
+          temp = temp->next;
+
+    }
+
+    if(L2!=NULL){
+        temp->next=L2;
+    }
+    else temp->next=L1; //remaining add
+
+    return d->next; // 0(d) -> ...merged list
+}
+
+ListNode* sortList(ListNode* head) {
         
-        multimap<int,ListNode*>m; //to store duplicat ekeys in sorted order
-        ListNode* temp=head;
-        while(temp!=NULL){
-            // m[temp->val]=temp;
-            m.insert({temp->val, temp});
-            temp=temp->next;
+//         For LeetCode 148 – Sort List, the expected O(n log n) solution is Merge Sort on the linked list.
+// The key idea is:
+// 1. Find the middle of the linked list.
+// 2. Split it into two halves.
+// 3. Recursively sort both halves.
+// 4. Merge the two sorted halves.
 
-        }
-        if(m.empty()) return NULL;
+// BC 
+if(head==NULL || head->next==NULL ){
+    return head;
+}
 
-        ListNode* nh=NULL;
-        ListNode* t=NULL;
+//find midle 
+ListNode* slow=head;
+ListNode* fast =head->next;
+while(fast!=NULL && fast->next!=NULL){
+    slow=slow->next;
+    fast=fast->next->next;
 
-   
-        for(auto p:m){
-
-            if(nh==NULL){
-                nh=p.second;
-                t=nh;
-                
-            }
-            else{
-                t->next=p.second;
-                t=t->next;
-            }
-
-        }
-        t->next=NULL;
+}
+//slow points to 1st middle 
+ListNode* l2=slow->next; //right part
+slow->next=NULL; //both lists are sepearated 
 
 
-return nh;
+//sort both parts 
+ListNode* l1=sortList(head); // first entire right part will be sorte dand merged 
+l2=sortList(l2); 
+//merge both parts 
+return merge(l1,l2);
+
     }
 };
